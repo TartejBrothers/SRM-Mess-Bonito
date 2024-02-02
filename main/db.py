c1521ae84e83193aab7ce030181b2efa1591ac56
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-02-02 10:09:29 +0530

    Updated: Build Setup

---
+++ main/db.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Mess-Details",
+        "USER": "tartejbrothers",
+        "PASSWORD": "GUMIV9jqt7Re",
+        "HOST": "ep-odd-hat-a13t3nv3.ap-southeast-1.aws.neon.tech",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
