8e862a505818d848c13f1ab962e95e5fb22459ed
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-05-11 10:36:31 +0530

    Added: Vercel Build

--- main/settings.py
+++ main/settings.py
@@ -101,14 +101,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "assets",
-]
+
 MEDIA_ROOT = BASE_DIR / "media"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
